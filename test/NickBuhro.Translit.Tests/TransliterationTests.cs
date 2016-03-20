0307ac3afcef8e131d56b3495b8f1b0b89e391f8
Nicholas Buhro <n.buhro@gmail.com>
2016-03-20 23:36:13 +0300

    Russian tests is prepared.

---
+++ test/NickBuhro.Translit.Tests/TransliterationTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace NickBuhro.Tests
+{
+    public sealed partial class TransliterationTests
+    {
+        private void RussianToLatinTestHelper(string source, string expectedResult)
+        {
+            var actual = Transliteration.CyrillicToLatin(source, Transliteration.Language.Russian);
+            Assert.Equal(expectedResult, actual);
+        }
+
+        private void LatinToRussianTestHelper(string source, string expectedResult)
+        {
+            var actual = Transliteration.LatinToCyrillyc(source, Transliteration.Language.Russian);
+            Assert.Equal(expectedResult, actual);
+        }
+    }
+}
