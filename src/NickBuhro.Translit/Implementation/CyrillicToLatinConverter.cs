b0c02d6095a4ec212c0e45b73a20e7713d1755bf
Nicholas Buhro <n.buhro@gmail.com>
2016-03-21 19:13:17 +0300

    CyrillicToLatin - fix for CZ/C rule.

--- src/NickBuhro.Translit/Implementation/CyrillicToLatinConverter.cs
+++ src/NickBuhro.Translit/Implementation/CyrillicToLatinConverter.cs
@@ -6,6 +6,7 @@ namespace NickBuhro.Translit.Implementation
 {
     internal partial struct CyrillicToLatinConverter
     {
+        private readonly Language _lang;
         private readonly string _src;
         private readonly Dictionary<char, string> _ruleSet;
 
@@ -24,6 +25,7 @@ namespace NickBuhro.Translit.Implementation
             Debug.Assert((int)Language.Macedonian == 5);
             
             _ruleSet = Rules[(int)lang];
+            _lang = lang;
             _src = source;
             _sb = null;
         }
@@ -41,20 +43,41 @@ namespace NickBuhro.Translit.Implementation
 
             _sb = new StringBuilder();
 
-            foreach (var srcChar in _src)
+            for (var srcIndex = 0; srcIndex < _src.Length; srcIndex++)
             {
                 string substitute;
-                if (_ruleSet.TryGetValue(srcChar, out substitute))
+                if (_ruleSet.TryGetValue(_src[srcIndex], out substitute))
                 {
+                    var nextChar = (_src.Length > (srcIndex + 1)) ? _src[srcIndex + 1] : ' ';
+                    substitute = CheckSpecificRules(substitute, nextChar);
                     _sb.Append(substitute);
                 }
                 else
                 {
-                    _sb.Append(srcChar);
+                    _sb.Append(_src[srcIndex]);
                 }
             }
 
             return _sb.ToString();
+        }
+
+
+        private static readonly HashSet<char> RuleCzCheck = new HashSet<char>
+        {
+            'Е', 'Ё', 'И', 'Й', 'I', 'Ы', 'Э', 'Ю', 'Я', 'е', 'ё', 'и', 'й', 'i', 'ы', 'э', 'ю', 'я',
+            'ѣ', 'Ѣ', 'ѵ', 'Ѵ'
+        };
+        private string CheckSpecificRules(string substitue, char nextSourceChar)
+        {
+            if (substitue.Length == 2)
+            {
+                // Ц	cz, c	cz, c	cz, c	cz, c	cz, c	рекомендуется использовать С перед буквами I, Е, Y, J; в остальных случаях CZ
+                if ((substitue[1] == 'z') && (RuleCzCheck.Contains(nextSourceChar)))
+                {
+                    return substitue.Substring(0, 1);
+                }
+            }
+            return substitue;
         }
     }
 }
