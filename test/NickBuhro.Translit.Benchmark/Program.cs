f1eb0e7e8d5a2614157f16303ee0f89149b81709
Nicholas Buhro <n.buhro@gmail.com>
2016-03-23 00:31:05 +0300

    Benchmark is added.
    It is implemented 1 test case with 2 measurements: for execution time
    and memory traffic.

--- test/NickBuhro.Translit.Benchmark/Program.cs
+++ test/NickBuhro.Translit.Benchmark/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Runtime;
 
 namespace NickBuhro.Translit.Benchmark
 {
@@ -12,63 +9,39 @@ namespace NickBuhro.Translit.Benchmark
     {
         public static void Main()
         {
-            long seed = Environment.TickCount;
-            Process.GetCurrentProcess().ProcessorAffinity = new IntPtr(2);
             Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
             Thread.CurrentThread.Priority = ThreadPriority.Highest;
 
-            Console.WriteLine("Cyrillic to Latin # 1");
-            ExecuteTest(() => Transliteration.CyrillicToLatin(TestCase.Value, Language.Russian));
+            ExecuteTest(
+                "Cyrillic to Latin",
+                () => Transliteration.CyrillicToLatin(TestCase.Value, Language.Russian));
 
-            Console.WriteLine("Latin to Cyrillic # 1");
-            ExecuteTest(() => Transliteration.LatinToCyrillyc(TestCase.Value, Language.Russian));
+            ExecuteTest(
+                "Latin to Cyrillic",
+                () => Transliteration.LatinToCyrillyc(TestCase.Value, Language.Russian));
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
         }
 
-        private static void ExecuteTest(Action func)
+        private static void ExecuteTest(string testName, Action testAction)
         {
-            const int I = 5;
-            const int N = 100;
-            
-            var elapsed = new long[I];
-            var mem = new long[I];
-            
-            var latencyMode = GCSettings.LatencyMode;
+            Console.WriteLine(testName);
+            Console.WriteLine();
 
-            var sw = new Stopwatch();
-            for (var i = 0; i < I; i++)
-            {   
-                sw.Reset();
-                func();
-
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-                GC.Collect();
-                GCSettings.LatencyMode = GCLatencyMode.LowLatency;
-
-                var memBefore = GC.GetTotalMemory(false);
-                             
-                sw.Start();
-
-                for (var j = 1; j < N; j++)
-                {
-                    func();
-                }
- 
-                sw.Stop();
-
-                elapsed[i] = sw.ElapsedMilliseconds;
-                mem[i] = GC.GetTotalMemory(false) - memBefore;
-                GCSettings.LatencyMode = latencyMode;
-            }
-            
-            for (var i = 0; i < I; i++)
+            var t = new double[5];
+            for (var i = 0; i < t.Length; i++)
             {
-                Console.WriteLine("    {0,5:0.00} ms {1,12:# ##0} B", (double)elapsed[i]/N, mem[i]/N);
+                t[i] = ExecutionTimeTest.Execute(testAction);
+                Console.Write("    {0:0.00} ms", t[i]);
             }
-            Console.WriteLine("AVG {0,5:0.00} ms {1,12:# ##0} B", (double)elapsed.Sum() / I / N, mem.Sum() / I / N);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("    Avg time:    {0,10:0.00} ms", t.Sum() / t.Length);
+            Console.WriteLine("    Mem traffic: {0,10:# ### ### ##0} B", MemoryTrafficTest.Execute(testAction));
+
             Console.WriteLine();
         }        
     }
---
+++ test/NickBuhro.Translit.Benchmark/Tests/MemoryTrafficTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NickBuhro.Translit.Benchmark
+{
+    public static class MemoryTrafficTest
+    {
+        private const long _512MB = 1 << 29;
+
+        public static long Execute(Action testAction, long memoryLimit = _512MB)
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            if (!GC.TryStartNoGCRegion(memoryLimit))
+                throw new InsufficientMemoryException();
+
+            try
+            {
+                var memBefore = GC.GetTotalMemory(false);
+
+                testAction();
+
+                return GC.GetTotalMemory(false) - memBefore;
+            }
+            finally
+            {           
+                GC.EndNoGCRegion();
+            }
+        }
+    }
+}
