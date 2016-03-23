8b77adb2cdc8eda6d26d2968e553ffee32c0acd3
Nicholas Buhro <n.buhro@gmail.com>
2016-03-23 18:05:14 +0300

    Tests refactoring. Round tests is added.

--- test/NickBuhro.Translit.Tests/TransliterationTests.cs
+++ test/NickBuhro.Translit.Tests/TransliterationTests.cs
@@ -4,16 +4,31 @@ namespace NickBuhro.Translit.Tests
 {
     public sealed partial class TransliterationTests
     {
-        private void RussianToLatinTestHelper(string source, string expectedResult)
+        private static void CyrillycToLatinTest(
+            Language lang,
+            string source, 
+            string expectedResult)
         {
-            var actual = Transliteration.CyrillicToLatin(source, Language.Russian);
+            var actual = Transliteration.CyrillicToLatin(source, lang);
+            Assert.Equal(expectedResult, actual);
+        }
+        
+        private static void LatinToCyrillicTest(
+            Language lang,
+            string source,
+            string expectedResult)
+        {
+            var actual = Transliteration.LatinToCyrillyc(source, lang);
             Assert.Equal(expectedResult, actual);
         }
 
-        private void LatinToRussianTestHelper(string source, string expectedResult)
+        private static void RoundTest(
+            Language lang,
+            string source)
         {
-            var actual = Transliteration.LatinToCyrillyc(source, Language.Russian);
-            Assert.Equal(expectedResult, actual);
+            var latin = Transliteration.CyrillicToLatin(source, lang);
+            var actual = Transliteration.LatinToCyrillyc(latin, lang);
+            Assert.Equal(source, actual);
         }
     }
 }
