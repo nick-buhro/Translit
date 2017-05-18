685ec8af0e217197b445e35caef9600a8580885b
Nicholas Buhro <bns@elko.by>
2017-05-18 16:41:11 +0000

    CI update.

--- coverage-script.bat
+++ coverage-script.bat
@@ -1,3 +1,3 @@
 ECHO OFF
 %USERPROFILE%\.nuget\packages\opencover\4.6.519\tools\OpenCover.Console.exe -register:user "-filter:+[NickBuhro.Translit]* -[*Tests]*" "-target:%USERPROFILE%\.nuget\packages\xunit.runner.console\2.2.0\tools\xunit.console.exe" "-targetargs:.\NickBuhro.Translit.Tests\bin\Debug\net46\NickBuhro.Translit.Tests.dll -noshadow"
-%USERPROFILE%\.nuget\packages\ReportGenerator\2.5.8\tools\ReportGenerator.exe "-reports:results.xml" "-targetdir:.\Coverage"
+%USERPROFILE%\.nuget\packages\ReportGenerator\2.5.8\tools\ReportGenerator.exe "-reports:results.xml" "-reporttypes:MHtml" "-targetdir:.\"
