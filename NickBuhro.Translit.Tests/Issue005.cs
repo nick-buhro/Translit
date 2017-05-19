e2f37ca3431f18b9878ae3d30f0dcddc2eadced3
Nicholas Buhro <bns@elko.by>
2017-05-19 17:09:41 +0000

    Issue #5 investigation.

---
+++ NickBuhro.Translit.Tests/Issue005.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace NickBuhro.Translit.Tests
+{
+    /// <summary>
+    /// Tests for <see href="https://github.com/nick-buhro/Translit/issues/5">GitHub Issue #5</see>.
+    /// </summary>
+    public sealed class Issue005
+    {
+        [Fact]
+        public void TestRu()
+        {
+            var actual = Transliteration.CyrillicToLatin("Obstacles", Language.Russian);
+            Assert.Equal("Obstacles", actual);
+        }
+
+        [Fact]
+        public void TestMk()
+        {
+            var actual = Transliteration.CyrillicToLatin("Obstacles", Language.Macedonian);
+            Assert.Equal("Obz`taclez`", actual);
+        }
+    }
+}
