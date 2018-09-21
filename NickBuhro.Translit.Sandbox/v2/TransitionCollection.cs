350b75b99be182219f80d63a565905245c8f5c02
Nicholas Buhro <n.buhro@gmail.com>
2018-09-21 12:16:03 +0300

    Remove Sandbox files. Add some tests.

--- NickBuhro.Translit.Sandbox/v2/TransitionCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace NickBuhro.Translit.Sandbox.v2
-{
-    internal sealed class TransitionCollection: Dictionary<char, Output>
-    {
-        public string DefaultOutputText { get; set; }
-    }
-}
--- NickBuhro.Translit.Tests/GeneralTests.cs
+++ NickBuhro.Translit.Tests/GeneralTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace NickBuhro.Translit.Tests
 {
@@ -30,6 +31,48 @@ namespace NickBuhro.Translit.Tests
         {
             var result = Transliteration.LatinToCyrillic("");
             Assert.Equal("", result);
+        }
+
+        [Fact]
+        public void C2LSimpleTest()
+        {
+            var result = Transliteration.CyrillicToLatin("Абв");
+            Assert.Equal("Abv", result);
+        }
+
+        [Fact]
+        public void L2CSimpleTest()
+        {
+            var result = Transliteration.LatinToCyrillic("Abv");
+            Assert.Equal("Абв", result);
+        }
+
+        [Fact]
+        public void C2LNumTest()
+        {
+            var result = Transliteration.CyrillicToLatin("123");
+            Assert.Equal("123", result);
+        }
+
+        [Fact]
+        public void L2CNumTest()
+        {
+            var result = Transliteration.LatinToCyrillic("123");
+            Assert.Equal("123", result);
+        }
+
+        [Fact]
+        public void C2LInvalidLanguageTest()
+        {
+            var lang = default(Language) - 1;
+            Assert.Throws<NotSupportedException>(() => Transliteration.CyrillicToLatin("123", lang));
+        }
+
+        [Fact]
+        public void L2CInvalidLanguageTest()
+        {
+            var lang = default(Language) - 1;
+            Assert.Throws<NotSupportedException>(() => Transliteration.LatinToCyrillic("123", lang));
         }
     }
 }
