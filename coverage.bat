9390be3b7bc954f4ac1d0ef442ed4f46e00acc44
Nicholas Buhro <bns@elko.by>
2022-09-18 10:35:51 +0300

    Fix code coverage builds.

--- coverage.bat
+++ coverage.bat
@@ -1,4 +1,4 @@
 ECHO OFF
-%USERPROFILE%\.nuget\packages\opencover\4.6.519\tools\OpenCover.Console.exe -register:user "-filter:+[NickBuhro.Translit]* -[*Tests]*" "-target:%USERPROFILE%\.nuget\packages\xunit.runner.console\2.4.0\tools\net461\xunit.console.exe" "-targetargs:.\NickBuhro.Translit.Tests\bin\Debug\net461\NickBuhro.Translit.Tests.dll -noshadow"
+%USERPROFILE%\.nuget\packages\opencover\4.6.519\tools\OpenCover.Console.exe -register:administrator "-filter:+[NickBuhro.Translit]* -[*Tests]*" "-target:%USERPROFILE%\.nuget\packages\xunit.runner.console\2.4.0\tools\net461\xunit.console.exe" "-targetargs:.\NickBuhro.Translit.Tests\bin\Debug\net461\NickBuhro.Translit.Tests.dll -noshadow"
 %USERPROFILE%\.nuget\packages\ReportGenerator\3.1.2\tools\ReportGenerator.exe "-reports:results.xml" "-reporttypes:MHtml" "-targetdir:.\"
 IF DEFINED COVERALLS_REPO_TOKEN (%USERPROFILE%\.nuget\packages\coveralls.io\1.4.2\tools\coveralls.net.exe --opencover results.xml --full-sources)
